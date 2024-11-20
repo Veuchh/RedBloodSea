9a8e6d50c5562a940f0f1ca986f723e629d95b49
AntoninLombard <antonin.lombard@hotmail.fr>
2024-11-20 16:26:48 +0100

    Add better options for Weakpoint handler and create the new weakpoint blueprint derived from C++

--- Source/RedBloodSea/Private/AC_WeakpointsManager.cpp
+++ Source/RedBloodSea/Private/AC_WeakpointsManager.cpp
@@ -4,6 +4,7 @@
 #include "AC_WeakpointsManager.h"
 
 #include "ComponentUtils.h"
+#include "Engine/SimpleConstructionScript.h"
 
 // Sets default values for this component's properties
 UAC_WeakpointsManager::UAC_WeakpointsManager()
@@ -14,7 +15,6 @@ UAC_WeakpointsManager::UAC_WeakpointsManager()
 
 	// ...
 }
-
 
 // Called when the game starts
 void UAC_WeakpointsManager::BeginPlay()
@@ -34,14 +34,30 @@ void UAC_WeakpointsManager::TickComponent(float DeltaTime, ELevelTick TickType, 
 	// ...
 }
 
+TArray<FName> UAC_WeakpointsManager::GetAllWeakpointsSockets()
+{
+	TArray<FName> result;
+	if(skeleton == nullptr)
+		return result;
+	 for (auto name : skeleton->GetAllSocketNames())
+	 {
+	 	if(name.ToString().StartsWith("wp_"))
+	 	{
+	 		result.Add(name);
+	 	}
+	 }
+	return result;
+}
+
 void UAC_WeakpointsManager::CreateWeakPoints()
 {
-	int WeakpointsSocketNb = 0;
-	USkeletalMeshComponent* skeleton = Cast<USkeletalMeshComponent>(owner->GetComponentByClass(USkeletalMeshComponent::StaticClass()));
-	TArray<FWeakPoint*> usedSockets;
+	owner = GetOwner();
+	skeleton = Cast<USkeletalMeshComponent>(owner->GetComponentByClass(USkeletalMeshComponent::StaticClass()));
 	int count = 0;
-	for (auto WeakPoint : WeakpointsSockets)
+	for (auto const & WeakPoint : WeakpointsSockets)
 	{
+		if(WeakPoint.bIsUsed)
+			continue;
 		switch (WeakPoint.Type)
 		{
 		case EWeakpointType::WPE_1:
@@ -55,56 +71,62 @@ void UAC_WeakpointsManager::CreateWeakPoints()
 			break;
 		}
 	}
-
-	int randomInt = FMath::RandRange(0, count - 1);
-	for (auto WeakPoint : WeakpointsSockets)
+	
+	for (int i = 0; i < Nb_weakpoints; i++)
 	{
-		if(WeakPoint.bIsUsed)
-			continue;
+		int randomInt = FMath::RandRange(0, count - 1);
+		for (auto & weakpoint : WeakpointsSockets)
+		{
+			if(weakpoint.bIsUsed)
+				continue;
 		
-		switch (WeakPoint.Type)
-		{
-		case EWeakpointType::WPE_1:
-			randomInt -= 10;
-			break;
-		case EWeakpointType::WPE_2:
-			randomInt -= 5;
-			break;
-		case EWeakpointType::WPE_3:
-			randomInt -= 1;
-			break;
-		}
-		if(randomInt < 0)
-		{
-			usedSockets.Add(&WeakPoint);
-			WeakPoint.bIsUsed = true;
-			switch (WeakPoint.Type)
+			switch (weakpoint.Type)
 			{
 			case EWeakpointType::WPE_1:
-				count -= 10;
+				randomInt -= 10;
 				break;
 			case EWeakpointType::WPE_2:
-				count -= 5;
+				randomInt -= 5;
 				break;
 			case EWeakpointType::WPE_3:
-				count -= 1;
+				randomInt -= 1;
 				break;
 			}
-			break;
+			if(randomInt < 0)
+			{
+				AttachWeakpoint(weakpoint.SocketName,weakpoint.MaxOffset);
+				weakpoint.bIsUsed = true;
+				switch (weakpoint.Type)
+				{
+				case EWeakpointType::WPE_1:
+					count -= 10;
+					break;
+				case EWeakpointType::WPE_2:
+					count -= 5;
+					break;
+				case EWeakpointType::WPE_3:
+					count -= 1;
+					break;
+				}
+				break;
+			}
 		}
 	}
 	
-	for (auto Element : usedSockets)
-	{
-		AActor* newActor = GetWorld()->SpawnActor(Weakpoint_BP);
-		newActor->AttachToComponent(skeleton,FAttachmentTransformRules::SnapToTargetNotIncludingScale,Element->SocketName);
-		UE::Math::TVector<double> offset = {
-			FMath::FRandRange(-Element->MaxOffset.X,Element->MaxOffset.X),
-			FMath::FRandRange(-Element->MaxOffset.Y,Element->MaxOffset.Y),
-			FMath::FRandRange(-Element->MaxOffset.Z,Element->MaxOffset.Z)};
-		newActor->SetActorRelativeLocation(offset);
-	}
 }
+
+
+void UAC_WeakpointsManager::AttachWeakpoint(const FName& socketName,const UE::Math::TVector<double>& maxOffset )
+{
+	AActor* newActor = GetWorld()->SpawnActor(Weakpoint_BP);
+	newActor->AttachToComponent(skeleton,FAttachmentTransformRules::SnapToTargetNotIncludingScale,socketName);
+	UE::Math::TVector<double> offset = {
+		FMath::FRandRange(-maxOffset.X,maxOffset.X),
+		FMath::FRandRange(-maxOffset.Y,maxOffset.Y),
+		FMath::FRandRange(-maxOffset.Z,maxOffset.Z)};
+	newActor->SetActorRelativeLocation(offset);
+}
+
 
 	
 
